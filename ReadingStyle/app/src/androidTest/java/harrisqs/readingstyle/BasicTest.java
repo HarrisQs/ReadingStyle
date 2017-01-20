ef8576c79e879918685dd29946cf684e1596eed7
HarrisQs <harris32916@gmail.com>
2017-01-20 15:34:35 +0800

    init project and fix gitignore

---
+++ ReadingStyle/app/src/androidTest/java/harrisqs/readingstyle/BasicTest.java
@@ -0,0 +1,24 @@
+package harrisqs.readingstyle;
+/**
+ * Created by HarrisQs on 2017/1/20.
+ */
+import com.robotium.solo.Solo;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class BasicTest extends ActivityInstrumentationTestCase2<MainActivity>
+{
+    private Solo BasicTestSolo;
+    public BasicTest()
+    {
+        super(MainActivity.class);
+    }
+    public void setUp() throws Exception
+    {
+        BasicTestSolo = new Solo(getInstrumentation(), getActivity());
+    }
+    @Override
+    public void tearDown() throws Exception
+    {
+        BasicTestSolo.finishOpenedActivities();
+    }
+}
