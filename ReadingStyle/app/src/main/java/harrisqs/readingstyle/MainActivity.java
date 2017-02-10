8850dac04a34293367fdf2ecbd59fbbdf35ce227
HarrisQs <harris32916@gmail.com>
2017-02-10 10:10:35 +0800

    Complete

--- ReadingStyle/app/src/main/java/harrisqs/readingstyle/MainActivity.java
+++ ReadingStyle/app/src/main/java/harrisqs/readingstyle/MainActivity.java
@@ -1,7 +1,11 @@
 package harrisqs.readingstyle;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -10,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.Layout;
 import android.text.Spannable;
@@ -20,6 +25,7 @@ import android.text.style.AlignmentSpan;
 import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -30,6 +36,7 @@ import android.widget.Toast;
 import com.jaeger.library.StatusBarUtil;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private StarterApplication getData;
     private RecyclerView bookStoreCard; // 卡片要裝進這個view來顯示
+    private RecyclerView recycleForSearch;              // 搜尋用recycle
+    private RecyclerView recycleForFavorite;           // 我的最愛recycle
     private LinearLayout linearLayout;// 外面大框框 (從這個view丟進去)
     private TextView nameOrGuest;               // 有登入顯示姓名, 否則顯示 "訪客"
     private TextView emailOrSignIn;             // 有登入顯示email, 否則顯示 "點這裡登入"
@@ -134,7 +143,7 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.myFavorites:
                         mToolbar.setTitle(R.string.myFavorites);
                         menuItem.setChecked(true);
-                        //myFavorite();
+                        myFavorite();
                         break;
                     case R.id.about:
                         mToolbar.setTitle(R.string.about);
@@ -247,6 +256,8 @@ public class MainActivity extends AppCompatActivity {
     {
         linearLayout = (LinearLayout) findViewById(R.id.linearlayout);
         bookStoreCard = new RecyclerView(this);
+        recycleForSearch = new RecyclerView(this);
+        recycleForFavorite = new RecyclerView(this);
         Card myAdapter = new Card(id ,this, getData.getBookStoreCardName(), getData.getBookStoreCardCity(),
                 getData.getBookStoreCardAddr(), getData.getBookStoreCardTime(),
                 getData.getBookStoreCardImage(),getData.getBookStoreCardphone(),
@@ -277,6 +288,101 @@ public class MainActivity extends AppCompatActivity {
         setting.edit().putString("name", null).commit();
         setting.edit().putString("email", null).commit();
         setting.edit().putString("photoString", null).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // for search
+        MenuItem menuSearchItem = menu.findItem(R.id.my_search);
+
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menuSearchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                if(newText.isEmpty()) {
+                    linearLayout.removeView(bookStoreCard);   // overview
+                    linearLayout.removeView(aboutOrExplain);
+                    linearLayout.removeView(recycleForSearch);
+                    linearLayout.removeView(recycleForFavorite);
+                    linearLayout.addView(bookStoreCard);
+                }
+                else {
+                    linearLayout.removeView(bookStoreCard);   // overview
+                    linearLayout.removeView(aboutOrExplain);
+                    linearLayout.removeView(recycleForSearch);
+                    linearLayout.removeView(recycleForFavorite);
+                    ArrayList<String> searchAnswerName = new ArrayList<String>();   // 店名
+                    ArrayList<String> searchAnswerCity = new ArrayList<String>();   // 縣市
+                    ArrayList<String> searchAnswerAddress = new ArrayList<String>();   // 地址
+                    ArrayList<String> searchAnswerBusinessHours = new ArrayList<String>();   // 營業時間
+                    ArrayList<String> searchAnswerPicture = new ArrayList<String>();   // 圖片
+                    ArrayList<String> searchAnswerPhone = new ArrayList<String>();   // 地址
+                    ArrayList<String> searchAnswerArriveWay = new ArrayList<String>();   // 營業時間
+                    ArrayList<String> searchAnswerIntro = new ArrayList<String>();   // 圖片
+
+                    for(int i = 0; i < getData.getBookStoreCardName().size(); i++) {
+                        if(getData.getBookStoreCardName().get(i).indexOf(newText) != -1
+                                || getData.getBookStoreCardCity().get(i).indexOf(newText) != -1)
+                        {
+                            searchAnswerName.add(getData.getBookStoreCardName().get(i));
+                            searchAnswerCity.add(getData.getBookStoreCardCity().get(i));
+                            searchAnswerAddress.add(getData.getBookStoreCardAddr().get(i));
+                            searchAnswerBusinessHours.add(getData.getBookStoreCardTime().get(i));
+                            searchAnswerPicture.add(getData.getBookStoreCardImage().get(i));
+                            searchAnswerPhone.add(getData.getBookStoreCardphone().get(i));
+                            searchAnswerArriveWay.add(getData.getBookStoreCardarriveWay().get(i));
+                            searchAnswerIntro.add(getData.getBookStoreCardIntro().get(i));
+                        }
+                    }
+                    Card myAdapter = new Card(id , MainActivity.this, searchAnswerName,
+                            searchAnswerCity, searchAnswerAddress, searchAnswerBusinessHours
+                            , searchAnswerPicture, searchAnswerPhone, searchAnswerArriveWay
+                            , searchAnswerIntro);
+                    final LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+                    layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                    recycleForSearch.setLayoutManager(layoutManager);
+                    recycleForSearch.setAdapter(myAdapter);
+
+                    linearLayout.addView(recycleForSearch);
+                }
+
+                return false;
+            }
+        });
+
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    linearLayout.removeView(bookStoreCard);   // overview
+                    linearLayout.removeView(aboutOrExplain);
+                    linearLayout.removeView(recycleForSearch);
+                    linearLayout.removeView(recycleForFavorite);
+                    linearLayout.addView(bookStoreCard);
+                    searchView.setIconified(true);
+                }
+            }
+        });
+
+        // Assumes current activity is the searchable activity
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        // 這邊讓icon可以還原到搜尋的icon
+        searchView.setIconifiedByDefault(true);
+
+        return true;
     }
 
     @Override
@@ -315,8 +421,10 @@ public class MainActivity extends AppCompatActivity {
     private void aboutUs()
     {
         // 先移除所有的動態view
-        linearLayout.removeView(bookStoreCard);
+        linearLayout.removeView(bookStoreCard);   // overview
         linearLayout.removeView(aboutOrExplain);
+        linearLayout.removeView(recycleForSearch);
+        linearLayout.removeView(recycleForFavorite);
 
         String text = "\n 開發成員 : \n\n陳亮宇\n張弘瑜\n鍾羽函\n蘇柏丞\n謝宣緯\n\n\t資料來源 : 行政院文化局";
         SpannableString spannable = new SpannableString(text);
@@ -332,8 +440,10 @@ public class MainActivity extends AppCompatActivity {
     private void info()
     {
         // 先移除所有的動態view
-        linearLayout.removeView(bookStoreCard);
+        linearLayout.removeView(bookStoreCard);   // overview
         linearLayout.removeView(aboutOrExplain);
+        linearLayout.removeView(recycleForSearch);
+        linearLayout.removeView(recycleForFavorite);
 
         String text = "\n\n    這是一個彙整台灣獨立書店的app，資料取自文化局的開放資源，主要目的在於提供沒接觸過或是剛接觸獨立書店的民眾，可以找尋到自己生活周遭哪裡有獨立書店，該如何拜訪、參觀，並且了解各個獨立店家的特色";
         SpannableString spannable = new SpannableString(text);
@@ -351,6 +461,8 @@ public class MainActivity extends AppCompatActivity {
         // 先移除所有的動態view
         linearLayout.removeView(bookStoreCard);   // overview
         linearLayout.removeView(aboutOrExplain);
+        linearLayout.removeView(recycleForSearch);
+        linearLayout.removeView(recycleForFavorite);
 
         // 再新增自己的view
         linearLayout.addView(bookStoreCard);
@@ -371,6 +483,63 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtras(bundle);
         startActivity(intent);
         overridePendingTransition(R.anim.left_in_2, R.anim.left_out_2);
+
+    }
+
+    // 我的最愛
+    private void myFavorite() {
+        // 先移除所有的動態view
+        linearLayout.removeView(bookStoreCard);   // overview
+        linearLayout.removeView(aboutOrExplain);
+        linearLayout.removeView(recycleForSearch);
+        linearLayout.removeView(recycleForFavorite);
+
+        DBHelper myFavorites = new DBHelper(this);
+
+        ArrayList<String> searchAnswerName = new ArrayList<String>();   // 店名
+        ArrayList<String> searchAnswerCity = new ArrayList<String>();   // 縣市
+        ArrayList<String> searchAnswerAddress = new ArrayList<String>();   // 地址
+        ArrayList<String> searchAnswerBusinessHours = new ArrayList<String>();   // 營業時間
+        ArrayList<String> searchAnswerPicture = new ArrayList<String>();   // 圖片
+        ArrayList<String> searchAnswerPhone = new ArrayList<String>();   // 地址
+        ArrayList<String> searchAnswerArriveWay = new ArrayList<String>();   // 營業時間
+        ArrayList<String> searchAnswerIntro = new ArrayList<String>();   // 圖片
+
+        Cursor cr = myFavorites.queryMyFavorite();
+
+        // 沒有資料表示沒有任何一家加入最愛過
+        if(cr.getCount() == 0) {
+            //Toast.makeText(StoreInfoActivity.this, "沒資料QQ", Toast.LENGTH_SHORT).show();
+        }
+
+        //
+        else {
+            cr.moveToFirst();
+            do {
+                int index = cr.getInt(2);
+                searchAnswerName.add(getData.getBookStoreCardName().get(index));
+                searchAnswerCity.add(getData.getBookStoreCardCity().get(index));
+                searchAnswerAddress.add(getData.getBookStoreCardAddr().get(index));
+                searchAnswerBusinessHours.add(getData.getBookStoreCardTime().get(index));
+                searchAnswerPicture.add(getData.getBookStoreCardImage().get(index));
+                searchAnswerPhone.add(getData.getBookStoreCardphone().get(index));
+                searchAnswerArriveWay.add(getData.getBookStoreCardarriveWay().get(index));
+                searchAnswerIntro.add(getData.getBookStoreCardIntro().get(index));
+
+            } while(cr.moveToNext());
+
+
+            Card myAdapter = new Card(id , MainActivity.this, searchAnswerName,
+                    searchAnswerCity, searchAnswerAddress, searchAnswerBusinessHours
+                    , searchAnswerPicture, searchAnswerPhone, searchAnswerArriveWay
+                    , searchAnswerIntro);
+            final LinearLayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
+            layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recycleForFavorite.setLayoutManager(layoutManager);
+            recycleForFavorite.setAdapter(myAdapter);
+
+            linearLayout.addView(recycleForFavorite);
+        }
 
     }
 }
